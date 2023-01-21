541fefa3faf3b54984c9af6f7e3f434ca6f2e3f6
Uriah Sanders <uriahsanders@christianengineeringsolutions>
2023-01-20 21:27:31 -0800

    Tab deletion now works

--- dist/js/header.js
+++ dist/js/header.js
@@ -7,9 +7,45 @@ $(function(){
     }
     var num_tabs = parseInt(localStorage.getItem('num_tabs'));
     function addTab(num){
+        if(localStorage.getItem('tab_' + num) === null){
+            return false;
+        }
         let tab_data = JSON.parse(localStorage.getItem('tab_' + num));
+        if(tab_data === null) return false;
         $('#new_tab').before(' <li id="tab_'+(num)+'" data-href="'+tab_data.href+'" class="tab">'+tab_data.text+'</li> ');
     }
+    $(document).on('click', '.tab_delete', function(e){
+        var thiz = $(this);
+        //id of tab to be deleted
+        var _id = thiz.parent().attr('id');
+        var _num = _id.split('_')[1];
+        //thiz.remove();
+        var num_tabss = $('.tab').length;
+        $('.tab').each(function(){
+            //id of tab
+            let tabId = $(this).attr('id');
+            let tabNum = tabId.split('_')[1];
+            if(localStorage.getItem('tab_' + tabNum) === null){
+                return false;
+            }
+            if(tabNum > _num){
+                //push back tab ids
+                $('#tab_' + tabNum).attr('id', '#tab_' + tabNum - 1);
+                let tabData = JSON.parse(localStorage.getItem('tab_' + tabNum));
+                let text = tabData.text;
+                let href = tabData.href;
+                localStorage.setItem('tab_' + tabNum - 1, JSON.stringify({text: text, href: href}));
+                //remove last tab
+                if(tabNum == num_tabss - 1){
+                    localStorage.setItem('tab_' + tabNum, null);
+                }
+            }
+        });
+        //remove deleted tab
+        localStorage.setItem('tab_' + _num, null);
+        $(this).parent().remove();
+        e.stopPropagation();
+    });
     $(document).on('click', '#new_tab', function(){
         let tab_num = num_tabs + 1; 
         localStorage.setItem('num_tabs', tab_num);
@@ -21,21 +57,19 @@ $(function(){
         addTab(tab_num);
         console.log(localStorage.getItem('num_tabs'));
     });
-    // if($('.tab').length - 1 === 0){
-    //     $('#new_tab').click();
-    // }
+    //populate tabs from localStorage
     for(let i = 1; i <= num_tabs; ++i){
         addTab(i);
         let tab_data = JSON.parse(localStorage.getItem('tab_' + i));
         if(tab_data !== null){
-            $('#tab_' + i).text(tab_data.text);
+            $('#tab_' + i).html('<span class="tab_delete">X</span>' + tab_data.text);
             $('#tab_' + i).data('href', tab_data.href);
         }
     }
     let active_tab_id = localStorage.getItem('active_tab');
     let active_tab_data = JSON.parse(localStorage.getItem(active_tab_id));
     if(active_tab_data !== null){
-        $('#' + active_tab_id).text(active_tab_data.text);
+        $('#' + active_tab_id).html('<span class="tab_delete">X</span>' + active_tab_data.text);
         $('#' + active_tab_id).data('href', active_tab_data.href);
     }
     if(localStorage.getItem('active_tab') === null){
@@ -46,6 +80,12 @@ $(function(){
         window.location.href = "/loginform";
     });
     $(document).on('click', '#home_link', function(){
+        var title = 'Home';
+        let href = '/';
+        $('.active_tab').html('<span class="tab_delete">X</span>' + title);
+        let tab_id = $('.active_tab').attr('id');
+        localStorage.setItem(tab_id, JSON.stringify({text: title, href: href}));
+        localStorage.setItem('active_tab', tab_id);
         window.location.href = "/";
     });
     $(document).on('click', '#profile_link', function(){
@@ -66,7 +106,7 @@ $(function(){
         },
         tolerance: 'pointer'
     });
-    $(document).on('click', '.tab', function(){
+    $(document).on('click', '.tab:not(".tab_delete")', function(){
         var href = window.location.href.split('/');
         let title = href[href.length - 2];
         let passage_id = href[href.length - 1];
