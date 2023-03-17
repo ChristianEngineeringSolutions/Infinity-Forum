8c6d367f9e8a1846ecbc26f30ef8a67563138290
Uriah Sanders <uriahsanders@christianengineeringsolutions>
2023-03-16 17:40:04 -0700

    Update to terms and preparation for bashfile

--- scripts.sh
+++ scripts.sh
@@ -1,1 +1,10 @@
 #Bash scripts to help with some stuff
+
+# Installing and configuration here
+
+# git clone
+# npm install
+# install mongod
+# sudo systemctl start mongod
+# npx nodemon
+# pm2/nginx for production
