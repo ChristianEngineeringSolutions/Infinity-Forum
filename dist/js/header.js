6f394f5a0c9d28f72ddc5d4f63f6ee70d78cde30
Uriah Sanders <uriahsanders@christianengineeringsolutions>
2023-03-25 10:20:34 -0700

    Close menu upon off click

--- dist/js/header.js
+++ dist/js/header.js
@@ -115,6 +115,17 @@ $(function(){
     $(document).on('click', '#menu-button', function(){
         $('#left-side-panel').show()
     });
+    $(document).on('click', function(e){
+        var container = $("#left-side-panel");
+        if(container.is(':visible') && e.target.id != 'menu-button'){
+            // if the target of the click isn't the container nor a descendant of the container
+            if (!container.is(e.target) && container.has(e.target).length === 0) 
+            {
+                container.hide();
+            }
+            e.stopPropagation();
+        }
+    });
     $(document).on('click', '#menu-close', function(){
         $('#left-side-panel').hide()
     });
