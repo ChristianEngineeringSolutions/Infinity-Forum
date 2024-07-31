a90e83537b888475b22e73efe6d24fd5d36795ee
Uriah Sanders <uriahsanders@christianengineeringsolutions>
2024-07-31 13:55:15 -0700

    Fixed leaderboard display of ranks on search and paginate; no longer jumping to top of page when clicking passage settings

---
+++ dist/js/leaderboard.js
@@ -0,0 +1,16 @@
+var page = 1;
+$(function(){
+	$(document).on('click', '#view-more-leaders', function(){
+		$.ajax({
+			type: 'get',
+			url: '/leaderboard',
+			data: {page: ++page},
+			success: function(data){
+				$('#leaders').append(data);
+				if($('#lead-num-1').length < 1){
+					$('.lead-num').hide();
+				}
+			}
+		});
+	});
+});
