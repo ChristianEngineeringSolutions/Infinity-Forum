44c41b89442574da02781f5b1b90c6f3eef7490a
Uriah Sanders <uriahsanders@christianengineeringsolutions>
2024-12-10 21:41:28 -0800

    True infinite scroll enabled on stream

--- dist/js/book.js
+++ dist/js/book.js
@@ -293,6 +293,46 @@ $(function(){
         $("html, body").animate({ scrollTop: $(document).height() - 1300 }, "slow");
     });
 
+    function isElementInViewport (el) {
+
+        // Special bonus for those using jQuery
+        if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+        }
+
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+        );
+    }
+
+    function onVisibilityChange(el, callback) {
+        var old_visible;
+        return function () {
+            var visible = isElementInViewport(el);
+            if (visible != old_visible) {
+                old_visible = visible;
+                if (typeof callback == 'function') {
+                    if(visible){
+                        callback();
+                    }
+                }
+            }
+        }
+    }
+
+    var handler = onVisibilityChange($('#view_more'), function() {
+        $('#view_more').click();
+    });
+
+
+    // jQuery
+    $(window).on('DOMContentLoaded load resize scroll', handler);
+
     // $(document).on('click', '[id^=star_]', function(){
     //     var _id = $(this).attr('id').split('_')[1];
     //     var thiz = $(this);
