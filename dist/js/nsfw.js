73526db59793fe8b5b0d45c3e75708ba5ab991db
Uriah Sanders <uriahsanders@christianengineeringsolutions>
2025-02-02 14:52:42 -0800

    Actually implemented safemode

--- dist/js/nsfw.js
+++ dist/js/nsfw.js
@@ -1,25 +1,28 @@
 function analyzeVideos(){
-  $('.uploadedVideo:not(.nsfw-checked)').each(function(){
-    const filter = new VideoFilter({
-      warningMessage: 'Content Warning: This video may contain sensitive material',
-      blurAmount: '15px',
-      sampleInterval: 5000, // Check every 5 second
-      predictionThreshold: 0.5 // 50% confidence threshold
-    });
+  if($('#safe-mode').val() == 'true'){
+    $('.uploadedVideo:not(.nsfw-checked)').each(function(){
+      const filter = new VideoFilter({
+        warningMessage: 'Content Warning: This video may contain sensitive material',
+        blurAmount: '15px',
+        sampleInterval: 5000, // Check every 5 second
+        predictionThreshold: 0.5 // 50% confidence threshold
+      });
 
-    filter.setupVideoAnalysis(this).catch(console.error);
-  });
+      filter.setupVideoAnalysis(this).catch(console.error);
+    });
+  }
 }
 function analyzeImages() {
-  console.log('one day');
-  $('.uploadedImage').each(function() {
-    console.log('getting there');
-    const filter = new ImageFilter({
-      warningMessage: 'Content Warning: This image may contain sensitive material',
-      blurAmount: '15px',
-      predictionThreshold: 0.5
+  if($('#safe-mode').val() == 'true'){
+    $('.uploadedImage').each(function() {
+      console.log('getting there');
+      const filter = new ImageFilter({
+        warningMessage: 'Content Warning: This image may contain sensitive material',
+        blurAmount: '15px',
+        predictionThreshold: 0.5
+      });
+      
+      filter.setupImageAnalysis(this).catch(console.error);
     });
-    
-    filter.setupImageAnalysis(this).catch(console.error);
-  });
+  }
 }
