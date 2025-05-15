d11e310fe0bd0785ee04907afd6a70ab77986901
Uriah Sanders <uriahsanders99@gmail.com>
2025-05-15 04:06:30 +0000

    Not sure

--- rs.sh
+++ rs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-pm2 stop sasame
-pm2 delete sasame
-pm2 kill
-pm2 start sasame.js
+#pm2 start sasame.js -i 2 -e "PORT=3000,PORT=3001"
+#pm2 stop sasame
+#pm2 delete sasame
+#pm2 kill
+#pm2 start sasame.js
+pm2 reload sasame
 sudo /usr/bin/systemctl reload nginx
